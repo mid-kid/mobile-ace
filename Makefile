ab7d7cb1707bbe91fa750e6596a3b06594715fc2
mid-kid <esteve.varela@gmail.com>
2019-09-04 16:34:27 +0200

    Initial commit
    Added warp mail for US Crystal

---
+++ Makefile
@@ -0,0 +1,19 @@
+RGBASM ?= rgbasm
+PYTHON3 ?= python3
+
+MAILS := crystal_us_trade crystal_us_battle
+
+.PHONY: all
+all: $(addsuffix .bin, $(MAILS))
+
+.PHONY: clean
+clean:
+	rm -f $(addsuffix .bin, $(MAILS)) $(addsuffix .o, $(MAILS))
+
+%.bin: %.o
+	@echo "$(PYTHON3) -m rgbbin $<"
+	@(cd rgbbin; $(PYTHON3) -m rgbbin -o ../ ../$<)
+
+%.o: %.asm
+	@echo "$(RGBASM) -o $@ $<"
+	@(cd pokecrystal; $(RGBASM) -o ../$@ ../$<)
---
+++ crystal_us_battle.asm
@@ -0,0 +1,56 @@
+INCLUDE "charmap.asm"
+INCLUDE "macros/enum.asm"
+INCLUDE "macros/scripts/maps.asm"
+INCLUDE "constants/gfx_constants.asm"
+INCLUDE "constants/text_constants.asm"
+INCLUDE "constants/item_constants.asm"
+INCLUDE "constants/item_data_constants.asm"
+INCLUDE "constants/map_constants.asm"
+
+RSSET $D002
+wTempMail EQU _RS
+wTempMailMessage RB MAIL_MSG_LENGTH
+wTempMailMessageEnd RB 1
+wTempMailAuthor RB PLAYER_NAME_LENGTH
+wTempMailAuthorNationaity RB 2
+wTempMailAuthorID RW 1
+wTempMailSpecies RB 1
+wTempMailType RB 1
+
+SECTION "crystal_us_battle", ROM0[wTempMail]
+Mail:
+    db $15, $0A, $C0, $00  ; Set up ACE from RunMobileScript
+    ; Generates the following at $CD52:
+    ; ld l, $C5
+    ; nop
+    ; ret nz
+
+    ld a, 1
+    ld c, a
+    ld hl, .warp_dest
+    call $22D1  ; CopyWarpData.skip+1
+    ld a, $25
+    ld hl, $6C38  ; FallIntoMapScript
+
+.warp_dest
+    map_id MOBILE_BATTLE_ROOM
+
+.end_message
+rept (wTempMailAuthor - wTempMail) - (Mail.end_message - Mail)
+    db "@"
+endr
+
+    db "REON Battl"
+
+.end_author
+;rept (wTempMailAuthorNationaity - wTempMail) - (Mail.end_author - Mail)
+    ;db "@"
+;endr
+    ;db "@@"  ; Author Nationality
+rept (wTempMailAuthorID - wTempMail) - (Mail.end_author - Mail)
+    db "@"
+endr
+
+    dw 0  ; Author ID
+    db 0  ; Author Species
+    db BLUESKY_MAIL  ; Mail Type
